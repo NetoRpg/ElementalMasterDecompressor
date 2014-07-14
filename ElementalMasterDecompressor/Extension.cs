dbda330f4f464db798caf7dd5dffd71c11b71470
NetoRpg <NTOProducoes@gmail.com>
2014-07-14 00:23:49 -0300

    1st Release

---
+++ ElementalMasterDecompressor/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ElementalMasterDecompressor
+{
+    static class Extension
+    {
+
+        public static int GetInt32(this byte[] bytes, int index = 0)
+        {
+            return (bytes[index + 0] << 24) + (bytes[index + 1] << 16) + (bytes[index + 2] << 8) + bytes[index + 3];
+        }
+
+        public static byte[] extractPiece(this MemoryStream ms, int offset, int length, int changeOffset = -1)
+        {
+            if (changeOffset > -1) ms.Position = changeOffset;
+
+            byte[] data = new byte[length];
+            ms.Read(data, 0, length);
+
+            return data;
+        }
+
+
+    }
+}
---
+++ ElementalMasterDecompressor/Main.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ElementalMasterDecompressor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Elemental Master ROM|*.bin";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+
+                    using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(ofd.FileName)))
+                    {
+                        int blockPosition = 0x7C000;
+                        ms.Position = blockPosition + 1;
+
+                        int nFiles = ms.ReadByte();
+                        int textBlockPointer = ms.extractPiece(0, 4).GetInt32();
+                        int textBlockDecSize = ms.extractPiece(0, 4).GetInt32();
+                        int textBlockComSize = ms.extractPiece(0, 4).GetInt32();
+
+                        ms.Position = blockPosition + textBlockPointer;
+
+                        string text = String.Empty;
+
+                        for (int i = 0; i < textBlockComSize; i++)
+                        {
+                            int currentByte = ms.ReadByte();
+
+                            for (int j = 0; j < 8; j++)
+                            {
+                                if (text.Length >= textBlockDecSize) break;
+
+                                if (((currentByte >> j) & 1) == 1)
+                                {
+                                    text += Convert.ToChar(ms.ReadByte());
+                                }
+                                else
+                                {
+                                    int basePosition = ms.ReadByte();
+                                    int size = ms.ReadByte();
+                                    int multiplier = (size >> 4) & 0x0F;
+                                    size &= 0x0F;
+
+                                    for (int k = 0; k < size + 3; k++)
+                                    {
+                                        int absolutePosition = 0x12 + basePosition + k + (0x100 * multiplier);
+
+                                        if (text.Length >= 0x1024 && absolutePosition + 0x1000 < text.Length)
+                                            absolutePosition += 0x1000;
+
+                                        if (absolutePosition < text.Length)
+                                            text += text.Substring(absolutePosition, 1);
+                                        else
+                                            text += " ";
+                                    }
+                                    i++;
+                                }
+                                i++;
+                            }
+                        }
+
+                        using (SaveFileDialog sfd = new SaveFileDialog())
+                        {
+
+                            sfd.Filter = "Text File|*.txt";
+
+                            if (sfd.ShowDialog() == DialogResult.OK)
+                            {
+                                using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                                {
+                                    sw.Write(text);
+                                }
+
+                                MessageBox.Show("Successfully Extracted");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
