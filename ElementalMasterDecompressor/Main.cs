2182de40fba38ffb824aae5f4ea8bdf5e2fbf28c
NetoRpg <NTOProducoes@gmail.com>
2014-07-16 16:33:35 -0300

    Commenting

--- ElementalMasterDecompressor/Main.cs
+++ ElementalMasterDecompressor/Main.cs
@@ -23,52 +23,84 @@ namespace ElementalMasterDecompressor
             {
                 ofd.Filter = "Elemental Master ROM|*.bin";
 
+                // Abre a janela para escolher o arquivo.
                 if (ofd.ShowDialog() == DialogResult.OK)
                 {
-
+                    // Carrega a ROM na memória.
                     using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(ofd.FileName)))
                     {
-                        int blockPosition = 0x7C000;
-                        ms.Position = blockPosition + 1;
+                        // Offset onde começa o header da compressão.
+                        int blockPosition = 0x7C000; 
 
+                        // Movendo o ponteiro para o offset do header + 1, para pular o primeiro byte.
+                        ms.Position = blockPosition + 1; 
+                        
+                        // Lê o número de arquivos.
                         int nFiles = ms.ReadByte();
-                        int textBlockPointer = ms.extractPiece(0, 4).GetInt32();
-                        int textBlockDecSize = ms.extractPiece(0, 4).GetInt32();
-                        int textBlockComSize = ms.extractPiece(0, 4).GetInt32();
 
+                        // Extrai 4 bytes e converte para um int.
+                        int textBlockPointer = ms.extractPiece(0, 4).GetInt32(); // Offset do bloco de texto
+                        int textBlockDecSize = ms.extractPiece(0, 4).GetInt32(); // Tamanho descomprimido
+                        int textBlockComSize = ms.extractPiece(0, 4).GetInt32(); // Tamanho comprimido
+
+                        // Como só tenho interesse no bloco de texto, pulo direto para o offset dele
+                        // ignorando as informações sobre os outros arquivos.
                         ms.Position = blockPosition + textBlockPointer;
 
+                        // Inicio a string que vai guardar o texto.
                         string text = String.Empty;
 
+                        // Loop no bloco de texto comprimido.
                         for (int i = 0; i < textBlockComSize; i++)
                         {
+                            // Lê um byte
                             int currentByte = ms.ReadByte();
 
+                            // Loop nos próximos 8 bytes.
                             for (int j = 0; j < 8; j++)
                             {
+                                // Se o tamanho da string de texto for maior ou igual o tamanho do arquivo descomprimido
+                                // o loop é interrompido.
                                 if (text.Length >= textBlockDecSize) break;
-
+                                
+                                // Faz o bitshift no byte que foi lido anteriormente e compara se ele é igual a 1
                                 if (((currentByte >> j) & 1) == 1)
                                 {
+                                    // Se for, lê um byte, converte para char e copia para a string.
                                     text += Convert.ToChar(ms.ReadByte());
                                 }
-                                else
+                                else // Se não:
                                 {
+                                    // Lê o primeiro byte do par.
                                     int basePosition = ms.ReadByte();
+
+                                    // Lê o segundo byte do par.
                                     int size = ms.ReadByte();
+
+                                    // Pega o primeiro nibble do segundo byte.
                                     int multiplier = (size >> 4) & 0x0F;
+
+                                    // Aplica a máscara no segundo byte para pegar apenas o segundo nibble.
                                     size &= 0x0F;
 
+                                    // Faz um loop da quantidade de bytes que serão copiados.
                                     for (int k = 0; k < size + 3; k++)
                                     {
+                                        // Calcula a posição
                                         int absolutePosition = 0x12 + basePosition + k + (0x100 * multiplier);
 
+                                        // Se o tamanho da string for maior ou igual a 0x1000 e o offset for menor que o tamanho da string
+                                        // soma 0x1000 ao offset.
                                         if (text.Length >= 0x1000 && absolutePosition + 0x1000 < text.Length)
                                             absolutePosition += 0x1000;
 
+                                        // Verifica se o offset é menor que o tamanho da string.
                                         if (absolutePosition < text.Length)
+                                        // Se sim, copia um caractere daquela posição da string.
                                             text += text.Substring(absolutePosition, 1);
                                         else
+                                        // Se não, adiciona um espaço na string.
+                                        // * Nota que isso só é usado no inicio, onde ele tenta descomprimir 0x24 espaços inexistentes.
                                             text += " ";
                                     }
                                     i++;
@@ -77,18 +109,21 @@ namespace ElementalMasterDecompressor
                             }
                         }
 
+                        
                         using (SaveFileDialog sfd = new SaveFileDialog())
                         {
 
                             sfd.Filter = "Text File|*.txt";
 
+                            // Abre a janela de Salvar Arquivo.
                             if (sfd.ShowDialog() == DialogResult.OK)
                             {
+                                // Salva o texto no local especificado.
                                 using (StreamWriter sw = new StreamWriter(sfd.FileName))
                                 {
                                     sw.Write(text);
                                 }
-
+                                // Exibe mensagem de sucesso.
                                 MessageBox.Show("Successfully Extracted");
                             }
                         }
--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-ElementalMasterDecompressor
+ Breve explicação sobre o algorítimo de descompressão:
 ===========================
-
-Decompress text from the game Elemental Master
+ 
+#### HEADER
+	O primeiro byte é 0x46, cuja função é desconhecida.
+	O segundo byte representa a quantidade de blocos de compressão. Nesse caso, 3.
+	A partir desse ponto os bytes são lidos em grupos de 4, em Big Endian. (Int32)
+	São três grupos para cada arquivo:
+		Os quatro primeiros bytes apontam para o offset onde começa o bloco de compressão, é um ponteiro relativo, portanto ele deve ser somado com o offset do inicio do header.
+		Os próximos quatro bytes representam o tamanho do arquivo após ser descomprimido.
+		Os últimos quatro bytes representam o tamanho do bloco comprimido.
+	E assim continua para cada arquivo.
+   
+---
+#### COMPRESSÃO
+	A compressão funciona da segunte forma:
+	Começa com um byte que indica se os próximos 8 bytes estão ou não comprimidos.
+	Cada bit desse byte é lido da direita para esquerda. 
+	Cada bit 0 indica um valor comprimido(um par de bytes) e cada bit 1 indica um valor descomprimido(um único byte).
+	O byte descomprimido pode ser copiado para o arquivo descompimido(sendo este um array ou uma string).
+	O par de bytes é lido da seguinte forma:
+		O primeiro byte deve ser somado a 0x12 e ao primeiro nibble do segundo byte. Este valor indica o offset, partindo do inicio do arquivo descomprimido, onde os bytes serão copiados. 
+		O segundo byte é dividido em dois nibbles. (Um nibble contém 4 bits).
+			Da esquerda pra direita, o primeiro nibble deve ser multiplicado por 0x100 e então somado ao primeiro byte.
+			O segundo nibble somado a 3 indica quantos bytes serão copiados a partir do offset conseguido anteriormente.
+	Ele usa uma janela de buffer de 0x1000(não tenho absoluta certeza, mas funciona). Quando o tamanho do arquvivo descomprimido for maior ou igual a 0x1000, esse mesmo valor deve ser somado ao offset que indica de onde os bytes serão copiados, mas se e apenas se o offset + 0x1000 for menor que o tamanho do arquivo descomprimido.
